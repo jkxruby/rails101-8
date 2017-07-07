a389710757bc35906a3301b17705120a5e36e4c2
jkxruby <jkxruby@gmail.com>
2017-07-07 13:56:35 +0800

    implement posts pagination with will_paginate

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,7 +15,7 @@ before_action :authenticate_user! , only: [:new, :create, :destroy, :update, :ed
 
   def show
     @group = Group.find(params[:id])
-    @posts = @group.posts.recent
+    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
   end
 
   def create
